4864de06b33605b09e020f9ce14e380c40bc9bf3
halning <Halning@ukr.net>
2017-03-24 19:28:25 +0200

    some fast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
+
+import { PerfectScrollbarModule } from 'angular2-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'angular2-perfect-scrollbar';
+
+import { AlertModule } from 'ng2-bootstrap';
+
+const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoreModule,
+    PerfectScrollbarModule.forRoot(PERFECT_SCROLLBAR_CONFIG),
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { WindowRefService } from './window-ref.service';
+
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [],
+    providers: [WindowRefService],
+})
+export class CoreModule {
+}
+
