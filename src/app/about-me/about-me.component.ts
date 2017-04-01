2e0dfd11b06f2f55e5eca9a4897e89013ffe1d7a
halning <Halningus@gmail.com>
2017-04-01 10:28:50 +0300

    replace img again

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -32,7 +32,7 @@ import {
         trigger('print', [
             state('void', style({width: '0', transform: 'steps(100, end)'})),
             transition(':enter', [
-                animate(3000, style({width: '100%', transform: 'steps(100, end)'}))
+                animate(2000, style({width: '100%', transform: 'steps(100, end)'}))
             ])
         ]),
         trigger('alive', [
