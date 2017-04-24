5e6ea1ea9565747e3cafc21becdb0e117166c902
halning <Halning@ukr.net>
2017-04-24 12:07:50 +0300

    fix about me, add to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from '@angular/core';
     selector: 'ha-footer',
     template: `
         <div class="footer container">
-            This site was created on Angular
+            This site was created on Angular<br>
+            This is a Progressive Web Application
             <p>2017 © Kyiv</p>
         </div>`,
     styleUrls: ['./footer.component.scss']
