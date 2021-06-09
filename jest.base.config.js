8d9b6a4cd09f58ae6c552485f0dd7e516d6eb9e6
Andrii_Khomenko <Andrii_Khomenko@epam.com>
2021-06-09 17:36:44 +0300

    Add separate jest configs

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,4 +1,4 @@
-// jest.config.js
+// jest.app.config.js
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyAwesomeLibModule } from 'my-awesome-lib';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MyAwesomeLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
