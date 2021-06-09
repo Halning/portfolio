50538a47de70ea573307e1f8c50add0aa9566a10
Andrii_Khomenko <Andrii_Khomenko@epam.com>
2021-06-09 17:18:58 +0300

    Add lib

---
+++ projects/my-awesome-lib/src/lib/my-awesome-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-awesome-lib',
+  template: `
+    <p>
+      my-awesome-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyAwesomeLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/my-awesome-lib/src/lib/my-awesome-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MyAwesomeLibComponent } from './my-awesome-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    MyAwesomeLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    MyAwesomeLibComponent
+  ]
+})
+export class MyAwesomeLibModule { }
