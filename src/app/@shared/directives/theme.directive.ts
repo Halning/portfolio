8e9f8adb7ba7d8d9faba010b712ff2e1234542bc
Andrii Khomenko <Andrii_Khomenko1@epam.com>
2021-07-02 10:17:09 +0300

    add theme directive

---
+++ src/app/@shared/directives/theme.directive.ts
@@ -0,0 +1,48 @@
+import { Directive, Input, HostBinding } from "@angular/core";
+
+@Directive({
+  selector: '[theme]',
+})
+export class ThemeDirective {
+  @Input()
+  theme: Record<string, string> = {};
+
+  @HostBinding('style')
+  get style(): string {
+    return JSON.stringify(this.theme)
+      .replace(/{|}|"/g, '')
+      .replace(/,--/g, ';--');
+  }
+}
+
+// usage
+
+// import { Component } from '@angular/core';
+//
+// @Component({
+//   selector: 'my-app',
+//   templateUrl: './app.component.html',
+//   styleUrls: [ './app.component.less' ]
+// })
+// export class AppComponent  {
+//   theme = 'default';
+//
+//   private readonly themes = {
+//     default: {},
+//     dark: {
+//       '--color-background': '#333',
+//       '--color-text': '#fff',
+//       '--color-button': '#777',
+//     },
+//     retrowave: {
+//       '--color-background': '#1b1837',
+//       '--color-text': '#3eb0ff',
+//       '--color-button': 'linear-gradient(45deg, #ce4039, #9836a6, #3d6bb1)',
+//       '--color-shadow': '0 0 3px #ff00ff, inset 0 0 2px 1px #ff00ff'
+//     }
+//   }
+//
+//   get currentTheme(): Record<string, string> {
+//     return this.themes[this.theme];
+//   }
+// }
--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NgLetDirective } from './directives/ng-let.directive';
 import { LetDirective } from './directives/let.directive';
 import { StickyDirective } from './directives/sticky.directive';
+import { ThemeDirective } from './directives/theme.directive';
 
 @NgModule({
   declarations: [
     NgLetDirective,
     LetDirective,
-    StickyDirective
+    StickyDirective,
+    ThemeDirective
   ],
   imports: [CommonModule],
 })
